f618872ad4f5f45c5ce76a88386ba07af0236092
imgzon3 <imgzon3@naver.com>
2021-07-18 01:48:11 +0900

    add IndexControllerTest.java (테스트 성공)

---
+++ src/test/java/com/webgrus17/everyscore/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.webgrus17.everyscore.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void main_page_loading() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("에브리스코어 테스트 화면");
+    }
+}
