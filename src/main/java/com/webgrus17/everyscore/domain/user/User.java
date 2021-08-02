a0ac80c781a20fc3d981a30e7c86636715f3258f
imgzon3 <imgzon3@naver.com>
2021-08-03 07:01:43 +0900

    User entity 수정

--- src/main/java/com/webgrus17/everyscore/domain/user/User.java
+++ src/main/java/com/webgrus17/everyscore/domain/user/User.java
@@ -6,25 +6,24 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-// 우선 스프링 시큐리티 사용하지 말지 결정이 안났으므로
-// user entity만 기본으로 만들어 둠
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor()
 @Entity // 해당 클래스 entity로 지정
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long code; // PK
-
-    @Column(unique = true, nullable = false)
-    private String id;
+    @GeneratedValue // pk
+    @Column(length = 20) // 최대 길이 제한 20
+    private String id; // 유저 아이디
 
     @Column(nullable = false)
-    private String password;
+    private String password; // 비밀번호
+
+    @Column(length = 10, nullable = false)
+    private String gender; // 성별
 
     @Column(nullable = false)
-    private String gender;
+    private String birth;
 
     @Column(nullable = false)
     private String name;
@@ -33,10 +32,11 @@ public class User {
     private String email;
 
     @Builder
-    public User(String id, String password, String gender, String name, String email) {
+    public User(String id, String password, String gender, String birth, String name, String email) {
         this.id = id;
         this.password = password;
         this.gender = gender;
+        this.birth = birth;
         this.name = name;
         this.email = email;
     }
