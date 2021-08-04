0c7ab062d2ff2d2cbfc3f53d210ab87f898ce626
imgzon3 <imgzon3@naver.com>
2021-08-04 19:29:46 +0900

    UserService에 save() 추가

--- src/main/java/com/webgrus17/everyscore/service/user/UserService.java
+++ src/main/java/com/webgrus17/everyscore/service/user/UserService.java
@@ -2,9 +2,11 @@ package com.webgrus17.everyscore.service.user;
 
 import com.webgrus17.everyscore.domain.user.User;
 import com.webgrus17.everyscore.domain.user.UserRepository;
+import com.webgrus17.everyscore.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,6 +20,20 @@ public class UserService implements UserDetailsService {
     public User loadUserByUsername(String Id) throws UsernameNotFoundException {
         return userRepository.findById(Id)
                 .orElseThrow(() -> new UsernameNotFoundException(Id));
+    }
+
+    public String save(UserDto userDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); // 인코더 선언
+        userDto.setPassword(encoder.encode(userDto.getPassword())); // 암호화
+
+        // db에 저장
+        return userRepository.save(User.builder()
+            .id(userDto.getId())
+            .password(userDto.getPassword())
+            .gender(userDto.getGender())
+            .birth(userDto.getBirth())
+            .name(userDto.getName())
+            .email(userDto.getEmail()).build()).getId();
     }
 
 }
