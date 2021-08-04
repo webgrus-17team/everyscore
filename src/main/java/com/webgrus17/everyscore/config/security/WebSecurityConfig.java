e733026c547814ed871eb352fe529e20bb78bbb6
imgzon3 <imgzon3@naver.com>
2021-08-04 18:49:39 +0900

    WebSecurityConfig에 메소드 추가

--- src/main/java/com/webgrus17/everyscore/config/security/WebSecurityConfig.java
+++ src/main/java/com/webgrus17/everyscore/config/security/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.webgrus17.everyscore.config.security;
 
+import com.webgrus17.everyscore.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // 우선 로그인, 로그아웃 성공시 넘어갈 url 비워두었으므로 실행시 오류 발생할 것
 @EnableWebSecurity // WebSecurity 활성화 어노테이션
+@RequiredArgsConstructor // final 생성자 추가
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService; // 유저 정보 가져올 서비스
 
     @Override
     public void configure(WebSecurity web) {
@@ -32,6 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .logoutSuccessUrl("") // 로그아웃 성공시 넘어갈 url, 프론트 주소 넣기(로컬이면 포트번호까지)
                         .invalidateHttpSession(true) // 로그아웃시 저장된 세션 날리기
         ;
+    }
+
+    @Override
+    // 로그인 시 필요한 정보
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(new BCryptPasswordEncoder()); // 패스워드 인코더 설정
     }
 
 }
