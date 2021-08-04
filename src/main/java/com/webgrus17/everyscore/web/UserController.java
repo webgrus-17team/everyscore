ff2ddf87641136e5eb391820a91aef303724cac6
imgzon3 <imgzon3@naver.com>
2021-08-04 19:36:16 +0900

    CORS 어노테이션 추가

--- src/main/java/com/webgrus17/everyscore/web/UserController.java
+++ src/main/java/com/webgrus17/everyscore/web/UserController.java
@@ -4,9 +4,11 @@ import com.webgrus17.everyscore.service.user.UserService;
 import com.webgrus17.everyscore.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@RequiredArgsConstructor
+@CrossOrigin(origins = "*") // CORS 설정, 우선 모든 도메인, 요청방식에 대해 허용하고 추후에 프론트 링크 넣어서 제한하기
+@RequiredArgsConstructor // 생성자
 @Controller
 public class UserController {
 
