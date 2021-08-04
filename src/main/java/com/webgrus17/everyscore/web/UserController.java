28fcf4c7bad0ca4d3feff9d7a8d27436fb65a998
imgzon3 <imgzon3@naver.com>
2021-08-04 19:39:20 +0900

    문구 수정

--- src/main/java/com/webgrus17/everyscore/web/UserController.java
+++ src/main/java/com/webgrus17/everyscore/web/UserController.java
@@ -17,6 +17,6 @@ public class UserController {
     @PostMapping("/api/v1/join") // 회원가입 api
     public String signup(UserDto userDto) {
         userService.save(userDto);
-        return ""; // 프론트 로그인 사이트로 연결할 것, 그냥 링크 적어도 되는지 찾아보기
+        return "redirect:"; // 프론트 로그인 사이트로 연결할 것, 그냥 링크 적어도 되는지 찾아보기
     }
 }
