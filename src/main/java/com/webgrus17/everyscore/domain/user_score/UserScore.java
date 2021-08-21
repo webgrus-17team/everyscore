836b33c17ff692b659626a4d4cd30a1da377df4a
LimHyeji <ihj028003@gmail.com>
2021-08-22 02:13:38 +0900

    USER SCORE FK 지정

--- src/main/java/com/webgrus17/everyscore/domain/user_score/UserScore.java
+++ src/main/java/com/webgrus17/everyscore/domain/user_score/UserScore.java
@@ -1,5 +1,7 @@
 package com.webgrus17.everyscore.domain.user_score;
 
+import com.webgrus17.everyscore.domain.subject.Subject;
+import com.webgrus17.everyscore.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,11 +14,17 @@ public class UserScore {
     @GeneratedValue(strategy= GenerationType.IDENTITY)  //PK, AUTO INCREMENT
     private Long id;
 
-    @Column(nullable=false) //FK 지정해야됨
-    private Long user_id;
+    @ManyToOne(fetch=FetchType.LAZY)  //user 1명이 user score 여러 개 만들 수 있음 //EAGER방식은 추적 어려움 문제로 변경
+    @JoinColumn(name="user_id") //fk 지정방식
+    private User user;
+    //@Column(nullable=false) //FK 지정해야됨
+    //private Long user_id;
 
-    @Column(nullable = false)   //FK 지정해야됨
-    private Long sub_id;
+    @ManyToOne(fetch=FetchType.LAZY)  //과목 1개에 user socre 여러 개 만들어질 수 있음
+    @JoinColumn(name="sub_id") //fk 지정방식
+    private Subject subject;
+    //@Column(nullable = false)   //FK 지정해야됨
+    //private Long sub_id;
 
     @Column(nullable = false)
     private Integer score;
