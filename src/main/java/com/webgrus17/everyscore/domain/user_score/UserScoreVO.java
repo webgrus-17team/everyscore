717c6224256aab8d5be0dcd3e31a4898c2ab52f2
LimHyeji <ihj028003@gmail.com>
2021-08-24 01:21:21 +0900

    jpa 집계 처리 위해 임의 생성

---
+++ src/main/java/com/webgrus17/everyscore/domain/user_score/UserScoreVO.java
@@ -0,0 +1,13 @@
+package com.webgrus17.everyscore.domain.user_score;
+
+public class UserScoreVO {
+    private Integer sampleSize;
+    private Double average;
+    private Double medianValue;
+    private Double averageOfLevel;
+    private Integer rank;
+
+    /*
+
+     */
+}
