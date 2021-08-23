c53f2ca460b20a85cc7f36b260f7aa60a0a09c25
LimHyeji <ihj028003@gmail.com>
2021-08-23 16:08:31 +0900

    우선 api 빨간줄 오류 해결

--- src/main/java/com/webgrus17/everyscore/web/controller/post/PostController.java
+++ src/main/java/com/webgrus17/everyscore/web/controller/post/PostController.java
@@ -1,12 +1,26 @@
 package com.webgrus17.everyscore.web.controller.post;
 
+import com.webgrus17.everyscore.domain.subject.Subject;
+import com.webgrus17.everyscore.domain.subject.SubjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 // 첫글 작성(교수명, 과목명 추가), 점수 입력 controller가 위치
 @CrossOrigin(origins = "http://localhost:8081")
 @RequiredArgsConstructor // 생성자
 @Controller
+//@RestController
 public class PostController {
+
+    private SubjectRepository subjectRepository;
+    @RequestMapping(value="/api/v1/start", method= RequestMethod.POST)
+    public ResponseEntity<?> postStart(@RequestBody Subject subject){
+        subjectRepository.save(subject);
+        return new ResponseEntity<>("{}", HttpStatus.CREATED);
+    }
+
+
 }
