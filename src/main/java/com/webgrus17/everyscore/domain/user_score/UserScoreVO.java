89d543ee2db57895af6b7f2102b23d797ecd79c0
LimHyeji <ihj028003@gmail.com>
2021-08-24 01:58:48 +0900

    jpa 쿼리 처리해야함

--- src/main/java/com/webgrus17/everyscore/domain/user_score/UserScoreVO.java
+++ src/main/java/com/webgrus17/everyscore/domain/user_score/UserScoreVO.java
@@ -1,5 +1,9 @@
 package com.webgrus17.everyscore.domain.user_score;
 
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+
 public class UserScoreVO {
     private Integer sampleSize;
     private Double average;
@@ -7,7 +11,11 @@ public class UserScoreVO {
     private Double averageOfLevel;
     private Integer rank;
 
-    /*
+    //위치를 여기에 할지, controller에서 request받은 직후에 할지 고민
+    //쿼리 내부 id_par과 sub_id_par는 프론트에서 받은 값으로, 아래와 같이 쓸 때 그 변수명이 실행될지 의문
+    @Query("SELECT x FROM UserScore x WHERE x.id=id_par, x.sub_id=sub_id_par")
+    UserScore find(String id_par, String sub_id_par){
 
-     */
+        //Collection<UserScore>
+    }
 }
