66ddaf0b8552b4afb26420604fad15bea498316d
imgzon3 <imgzon3@naver.com>
2021-08-26 05:57:44 +0900

    SubjectRepository에 jpa 메소드이름 쿼리 생성으로 추가

--- src/main/java/com/webgrus17/everyscore/domain/subject/SubjectRepository.java
+++ src/main/java/com/webgrus17/everyscore/domain/subject/SubjectRepository.java
@@ -2,4 +2,12 @@ package com.webgrus17.everyscore.domain.subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubjectRepository extends JpaRepository<Subject, Long> {}
+import java.util.List;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    // https://ppomelo.tistory.com/155?category=908484
+    // JPA 메소드 이름 쿼리 생성 링크
+
+    List<Subject> findBySubject_nameAndProfessor_name(String Subject_name, String Professor_name);
+}
