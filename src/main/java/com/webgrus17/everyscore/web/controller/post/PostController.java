33c298d017c5819513071d7d37aed9469769b29a
LimHyeji <ihj028003@gmail.com>
2021-08-27 00:24:38 +0900

    우선 첫글작성 주석 추가

--- src/main/java/com/webgrus17/everyscore/web/controller/post/PostController.java
+++ src/main/java/com/webgrus17/everyscore/web/controller/post/PostController.java
@@ -26,11 +26,11 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     //첫글작성
-    //시험종류 3가지 생성해야함
     /*
     테스트 실행 시에 500번 interver server error가 뜸
     db 접속에 불량이 있는것으로 보임
     레포지토리 save 코드를 제거했을 때엔 404번 포트가 뜸 즉, 이 코드에서 발생하는 문제로 보임
+    -->> 해결됨
      */
     private final SubjectService subjectService;
 
@@ -39,8 +39,9 @@ public class PostController {
         return subjectService.save(subjectSaveDto);
     }
     /*
-    1. 받은 값은 교수명과 과목명... 여기에 덧붙여 시험종류를 추가해줘야하는데 그렇게 되면 requestbody를 subjectsavedto로 처리하면
-        안될 것 같음
+    ***시험종류 3가지 추가하는 코드 있어야함
+    1. 받은 값은 교수명과 과목명... 여기에 덧붙여 시험종류를 추가해줘야하는데 그렇게 되면 requestbody를 subjectsavedto로 처리하면 안됨
+    ->대안 필요
     2. 시험종류 추가는 enum으로 처리해서 반복문 3번 돌리거나, 일일이 추가하거나 하면 될거 같음
      */
 
